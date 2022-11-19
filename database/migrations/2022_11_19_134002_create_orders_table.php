53ce30938948a151c84871f7de673b505b9d332a
MohamedGamal1 <mohamedgamal74@hotmail.com>
2022-11-20 00:41:21 +0200

    orders migration

---
+++ database/migrations/2022_11_19_134002_create_orders_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->text('from');
+            $table->text('to');
+            $table->date('appointment_date');
+            $table->time('appointment_time');
+            $table->string('image_front')->nullable();
+            $table->string('image_back')->nullable();
+            $table->string('image_ticket')->nullable();
+            $table->string('image_passport')->nullable();
+            $table->string('payment_method');
+            $table->enum('payment_status', ['pending', 'paid', 'failed'])->default('pending');
+            $table->enum('status', ['pending', 'processing', 'delivering', 'completed', 'cancelled', 'refunded'])->default('pending');
+            $table->float('total')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
