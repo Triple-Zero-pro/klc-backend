22ce9dad5f5b94a78bcd3e4e2de1269035f570da
MohamedGamal1 <mohamedgamal74@hotmail.com>
2022-11-23 01:10:10 +0200

    user credit migration

---
+++ database/migrations/2022_11_22_225953_create_user_credits_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_credits', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->string('payment_method')->nullable();
+            $table->integer('credit_number')->nullable();
+            $table->string('credit_name')->nullable();
+            $table->date('expired_date')->nullable();
+            $table->integer('cvv')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_credits');
+    }
+};
