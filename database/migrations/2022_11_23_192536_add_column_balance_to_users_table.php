0529655fe6d72af45cb4598e7f4c3c24cf70d810
MohamedGamal1 <mohamedgamal74@hotmail.com>
2022-11-23 23:46:32 +0200

    add balance column to user for credit

--- database/migrations/2022_11_23_192536_add_column_balance_to_users_table.php
+++ database/migrations/2022_11_23_192536_add_column_balance_to_users_table.php
@@ -28,6 +28,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
+            $table->dropColumn('balance');
         });
     }
 };
