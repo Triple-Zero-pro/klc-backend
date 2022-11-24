fa5471b27e00c66704d5e07817f90450f638b699
MohamedGamal1 <mohamedgamal74@hotmail.com>
2022-11-24 20:36:42 +0200

    servic order attributes

---
+++ database/migrations/2022_11_24_182443_create_order_service_attributes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_service_attributes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('service_id')->nullable()->constrained('services')->nullOnDelete();
+            $table->string('title_ar')->nullable();
+            $table->string('title_en')->nullable();
+            $table->string('input_name')->nullable();
+            $table->string('input_type')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_service_attributes');
+    }
+};
